370dff4102ab0f6c3be7459a14fa65d45a869832
sshujonn <toufiqueaman@gmail.com>
2023-05-15 22:03:42 +0600

    150523-01

--- ac_openning/views.py
+++ ac_openning/views.py
@@ -23,26 +23,29 @@ class NIDCardScriptLand(APIView):
         return Response({}, template_name=self.template_name)
 
     def post(self, request):
-        card_script = request.FILES.get('card_script')
-        n_card_script = request.FILES.get('n_card_script')
+        try:
+            card_script = request.FILES.get('card_script')
+            n_card_script = request.FILES.get('n_card_script')
 
-        form_fillup = request.POST.get('form_fillup')
+            form_fillup = request.POST.get('form_fillup')
 
-        handle_uploaded_file(card_script, 'nid.htm')
-        data = process_data("nid.htm")
-        data = {"customer": data}
-        message = update_or_insert_customer(data.get("customer"))
-        messages.warning(request, message)
-        request.session['c_nid'] = data.get("customer").get("national_id")
-        if n_card_script:
-            handle_uploaded_file(n_card_script,'n_nid.htm')
-            n_data = process_data("n_nid.htm")
-            data["nominee"] = n_data
-            n_message = update_or_insert_customer(data.get("nominee"))
-            messages.warning(request, n_message)
-            request.session['n_nid'] = data.get("nominee").get("national_id")
+            handle_uploaded_file(card_script, 'nid.htm')
+            data = process_data("nid.htm")
+            data = {"customer": data}
+            message = update_or_insert_customer(data.get("customer"))
+            messages.warning(request, message)
+            request.session['c_nid'] = data.get("customer").get("national_id")
+            if n_card_script:
+                handle_uploaded_file(n_card_script,'n_nid.htm')
+                n_data = process_data("n_nid.htm")
+                data["nominee"] = n_data
+                n_message = update_or_insert_customer(data.get("nominee"))
+                messages.warning(request, n_message)
+                request.session['n_nid'] = data.get("nominee").get("national_id")
 
-        return HttpResponseRedirect(reverse('show_data'))
+            return HttpResponseRedirect(reverse('show_data'))
+        except:
+            return HttpResponseRedirect(reverse('card_script'))
 
 
 class ShowAllData(APIView):
@@ -51,11 +54,14 @@ class ShowAllData(APIView):
     renderer_classes = [renderers.TemplateHTMLRenderer]
 
     def get(self, request):
-        nid =request.session['c_nid']
-        nnid =request.session.get('n_nid')
+        try:
+            nid =request.session['c_nid']
+            nnid =request.session.get('n_nid')
 
-        data = process_for_show_data(nid, nnid)
-        return Response({"data": data}, template_name=self.template_name)
+            data = process_for_show_data(nid, nnid)
+            return Response({"data": data}, template_name=self.template_name)
+        except:
+            return HttpResponseRedirect(reverse('card_script'))
 
 
 class FormFillup(APIView):
@@ -64,13 +70,16 @@ class FormFillup(APIView):
     renderer_classes = [renderers.TemplateHTMLRenderer]
 
     def post(self, request):
-        acc_dict, cust_dict, nom_dict, tp_dict = process_for_form_fillup(request.data)
+        try:
+            acc_dict, cust_dict, nom_dict, tp_dict = process_for_form_fillup(request.data)
 
-        acc_id, cust_id, nom_id, tp_id = update_db(acc_dict, cust_dict, nom_dict, tp_dict)
+            acc_id, cust_id, nom_id, tp_id = update_db(acc_dict, cust_dict, nom_dict, tp_dict)
 
-        file_link = fill_up_form(acc_id, cust_id, tp_id, nom_id)
-        request.session.clear()
-        return JsonResponse({"file_link": file_link}, safe=False)
+            file_link = fill_up_form(acc_id, cust_id, tp_id, nom_id)
+            request.session.clear()
+            return JsonResponse({"file_link": file_link}, safe=False)
+        except:
+            return HttpResponseRedirect(reverse('card_script'))
 
 
 
